b5073cbae62cfb7d08d1ed2642af51c9c1145f18
Daniel Fryar <daniel.fryar@gmail.com>
2020-05-21 13:41:43 -0500

    First commit, Spring Initializer

---
+++ src/main/java/com/codeup/dandihammocks/DandihammocksApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.dandihammocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandihammocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DandihammocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/dandihammocks/DandihammocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.dandihammocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DandihammocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
