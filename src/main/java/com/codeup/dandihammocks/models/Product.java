2c280fd0825ff29829acce9f3291429d046afeb5
Daniel Fryar <daniel.fryar@gmail.com>
2020-05-21 14:12:22 -0500

    Product class

---
+++ src/main/java/com/codeup/dandihammocks/models/Product.java
@@ -0,0 +1,96 @@
+package com.codeup.dandihammocks.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int priceInCents;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "integer default 1")
+    private int inventory;
+
+    private String imageUrl;
+
+    private String type;
+
+    public Product() {
+    }
+
+    public Product(Product copy) {
+        this.id = copy.id;
+        this.name = copy.name;
+        this.priceInCents = copy.priceInCents;
+        this.description = copy.description;
+        this.inventory = copy.inventory;
+        this.imageUrl = copy.imageUrl;
+        this.type = copy.type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriceInCents() {
+        return priceInCents;
+    }
+
+    public void setPriceInCents(int priceInCents) {
+        this.priceInCents = priceInCents;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
